5feb29344cc83630f4f3dc3bac62760abd059051
ihossen016 <ihossen016@gmail.com>
2023-03-01 21:11:03 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.css";
+
+const App: React.FC = () => {
+    return (
+        <div className="app">
+            <span className="heading">TodoList</span>
+        </div>
+    );
+};
+
+export default App;
